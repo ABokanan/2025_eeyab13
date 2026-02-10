2c3388900b2521387a3960a62dfe2b4285c73e99
ABokanan <eeyab13@nottingham.ac.uk>
2026-02-10 11:01:10 +0000

    files transfer

---
+++ worksheet3/adder.h
@@ -0,0 +1,23 @@
+#ifndef MATHSLIB_ADDER_H
+#define MATHSLIB_ADDER_H
+
+#if defined (WIN32) | defined (_WIN32)
+#ifdef maths_STATIC
+
+#define MATHSLIB_API
+#else
+
+#ifdef maths_EXPORTS
+#define MATHSSLIB_API __declspec ( dllexport )
+#else
+#define MATHSLIB_API __declspec ( dellimport )
+#endif
+#endif
+#else
+#define MATHSLIB_API
+
+#endif
+
+MATHSLIB_API int add (int a, int b);
+
+#endif
