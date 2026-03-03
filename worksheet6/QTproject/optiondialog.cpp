a02aca206d4e7f2a16c9ebedcc5c4717ee46ec6f
ABokanan <eeyab13@nottingham.ac.uk>
2026-03-03 10:15:20 +0000

    Updated optiondialog.cpp

--- worksheet6/QTproject/optiondialog.cpp
+++ worksheet6/QTproject/optiondialog.cpp
@@ -12,3 +12,18 @@ OptionDialog::~OptionDialog()
 {
     delete ui;
 }
+void OptionDialog::updateDialogFromPart(ModelPart* part) {
+    // 1. Get the name from the part (stored in column 0 of its QVariant array)
+    QString partName = part->data(0).toString();
+
+    // 2. Put that name into the text box on the dialog
+    ui->lineEdit->setText(partName);
+}
+
+void OptionDialog::updatePartFromDialog(ModelPart* part) {
+    // 1. Read whatever the user typed into the text box
+    QString newName = ui->lineEdit->text();
+
+    // 2. Save it back to the part (you might need to check ModelPart.h for the exact setter function, usually it's set() or modifying the QVariant directly)
+    part->set(0, newName);
+}
